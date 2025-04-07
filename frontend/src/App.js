e1b51d8eb3ab3d3bf1c8029d98585c158f78d135
Ankit Patil <ankitpatil9307@gmail.com>
2025-04-07 16:14:30 +0530

    third

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import AdminDashboard from "./pages/AdminDashboard";
-import SignUp from "./pages/signUp";
+import SignUp from "./pages/SignUp";
 import LoanCalculation from "./pages/LoanCalculation";
 import Total from "./pages/Total";
 function App() {
